551afdec638055434f40d616dd145262b6b0493c
kadir-zeren <k4dzeren@gmail.com>
2024-05-05 09:59:44 +0300

    boolen

---
+++ python.py
@@ -0,0 +1,137 @@
+print(3+5)
+print('3'+'5')
+print(3*5)
+print('abc'*3)
+print('3'+'abc')
+print(11-7)
+print(4-11.0)
+print('11-7')
+# print('4'+ 4)
+print('clarusway')
+print('3+5')
+print('!-**asflkasdjlfk')
+num1, num2=81,55
+num3 = num1-num2
+print(num3)
+a,b=3,5
+print(a,b)
+
+r=5
+pi=3.14
+area = pi * r ** 2
+print(area)
+print(11%2)
+print((4*5)/2)
+
+print(2**3)
+print(3**2)
+a=2
+b=8
+print((a*b)**0.5)
+a = 5
+a = a + 1
+a += 1
+print(a)
+
+A = 10
+A = A * 2
+print(A)
+
+A *= 2
+print(A)
+
+print(3 + 5 * 2)
+a = (1+1) ** (4**(2/2 * 2)/ 2)
+print(a)
+
+a = -3 ** 2
+print(a)
+
+a = (-3) ** 2
+print(a)
+
+a=3
+b=4
+hipotenus = (a**2+b**2)**0.5
+print(hipotenus)
+
+print(a,b,hipotenus)
+print(a,b,hipotenus,sep='----->')
+print(a)
+print(b)
+print(hipotenus)
+print(a,end='bitiiiii')
+print(b)
+print(hipotenus)
+
+bir = 'ilk satir'
+iki = 'ikinci satir'
+uc = 'ucuncu satir'
+
+print(bir,iki,uc)
+print(bir,iki,uc, sep='')
+print(bir,iki,uc, sep='____bosluk____')
+
+print(bir, end='\n****')
+print(iki, end='\n')
+print(uc)
+
+print(bir,iki,uc, sep='* - *',end='&')
+print(bir,iki,uc, sep='')
+print(bir,iki,uc, sep='')
+print(bir,iki,uc, sep='* - *',end='&\n')
+print(bir,iki,uc, sep='')
+print(bir,iki,uc, sep='')
+
+print('abc',555)
+
+print('C:\\north pole\noise_penguins.text')
+print('---------')
+print('first', 'second', 'third', sep='\t')
+
+text = 'Bu\gun\ben\neden\taksiye\bindim'
+print(text)
+
+print('Bu\gun\\ben\\neden\\taksiye\\bindim')
+
+print( r'Bu\gun\ben\neden\taksiye\bindim')
+
+print('we are','\boosting', 'our', '\brotherhood')
+print('it\'s essential to learn Python\' libraries in IT World')
+
+print(True and True)
+print(True and False)
+print(False and True and True and True)
+print(True or False or False)
+print(bool(False))
+print(bool(0))
+print(bool([]))
+print(bool(''))
+print(1 and 1 )
+print(1 and 0)
+print(bool('abcdeg'))
+print(bool(5))
+print(bool(None))
+
+print(not True)
+print(not False)
+print(not 'string')
+print(True and False or not False or False)
+print(True and True and True)
+print('string' and 5 and 6.2)
+print(5 and 0 and False and 'abcc')
+print(False or False or False or True)
+print(False or '' or 'False' or '35' or 45)
+
+print(2 and 3)
+print(1 and 0)
+
+print(2 and "hello world")
+print([] and "be happy!")
+print(None and ())
+
+print(2 or 'hello world')
+print([] or 'be happy!')
+print(None or ())
+print({} or 0)
+print({0} or False)
